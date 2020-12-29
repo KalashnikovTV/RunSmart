60a9cb0e1c42d047da30c925b6cc5dea0e4827ce
KalashnikovTV <max.malaschkov@mail.ru>
2020-12-29 16:32:37 +0300

    finished adaptive

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
